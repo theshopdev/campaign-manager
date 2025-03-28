e712188a626c7c802f725382bac1c682eed15777
Patrik Šimonič <patrik.simonic@icloud.com>
2025-03-28 21:46:29 +0100

    Added gift service to return UUIDs to retrieve

---
+++ src/Services/CampaignManagerGiftService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TheShop\CampaignManager\Services;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use TheShop\CampaignManager\Models\CampaignManagerUpsell;
+
+class CampaignManagerGiftService
+{
+    public static function getUUIDs(array $itemsInCart): array
+    {
+        return CampaignManagerUpsell::query()
+            ->whereNotIn('product_uuid', $itemsInCart)
+            ->where(function (Builder $query) {
+                $query->whereNull('valid_from')
+                    ->orWhereDate('valid_from', '>=', Carbon::today());
+            })
+            ->where(function (Builder $query) {
+                $query->whereNull('valid_to')
+                    ->orWhereDate('valid_to', '<=', Carbon::today());
+            })
+            ->orderByDesc('score')
+            ->pluck('product_uuid')->toArray();
+    }
+}
