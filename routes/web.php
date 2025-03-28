814254122d860cee2d92caac462408ed1cee285c
Patrik Šimonič <patrik.simonic@icloud.com>
2025-03-28 23:06:14 +0100

    Fixed session handling

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
 use TheShop\CampaignManager\Controllers\AuthController;
 use TheShop\CampaignManager\Controllers\DashboardController;
@@ -10,9 +9,7 @@ use TheShop\CampaignManager\Controllers\UpsellController;
 use TheShop\CampaignManager\Middleware\AuthMiddleware;
 
 Route::group([
-    'prefix' => 'campaign-manager',
-    'middleware' => [StartSession::class, AuthMiddleware::class],
-    'as' => 'campaign-manager.'
+    'middleware' => [AuthMiddleware::class],
 ], static function () {
     Route::resource('gift', GiftController::class);
     Route::resource('upsell', UpsellController::class);
@@ -21,11 +18,6 @@ Route::group([
     Route::get('/', DashboardController::class)->name('dashboard');
 });
 
-Route::group([
-    'prefix' => 'campaign-manager',
-    'as' => 'campaign-manager.'
-], static function () {
-    Route::post('/logout', [AuthController::class, 'APILogout'])->name('api.logout');
-    Route::post('/login', [AuthController::class, 'APILogin'])->name('api.login');
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-});
+Route::post('/logout', [AuthController::class, 'APILogout'])->name('api.logout');
+Route::post('/login', [AuthController::class, 'APILogin'])->name('api.login');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
--- src/CampaignManagerServiceProvider.php
+++ src/CampaignManagerServiceProvider.php
@@ -3,12 +3,21 @@
 namespace TheShop\CampaignManager;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class CampaignManagerServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/theshop-campaign-manager.php', 'theshop-campaign-manager');
+
+        Route::group([
+            'prefix' => 'campaign-manager',
+            'as' => 'campaign-manager.',
+            'middleware' => 'web',
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
 
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
