f95061369c329e55d82c2fd7e28c80d5acf0ce7b
Patrik Šimonič <patrik.simonic@icloud.com>
2025-03-29 21:50:33 +0100

    Added CampaignManagerGiftService

---
+++ src/Services/CampaignManagerGiftService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace TheShop\CampaignManager\Services;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+use TheShop\CampaignManager\Models\CampaignManagerGift;
+use TheShop\CampaignManager\Models\CampaignManagerUpsell;
+
+class CampaignManagerGiftService
+{
+    public static function getAvailableUUIDs(float $value, string $currency): array
+    {
+        $currency = Str::lower($currency);
+
+        if (!in_array($currency, config('theshop-campaign-manager.currencies'))) {
+            return [];
+        }
+
+        return CampaignManagerGift::query()
+            ->whereRaw("CAST(JSON_EXTRACT(minimum_spend, '$.$currency') AS UNSIGNED) >= ?", [$value])
+            ->whereRaw("CAST(JSON_EXTRACT(maximum_spend, '$.$currency') AS UNSIGNED) <= ?", [$value])
+            ->pluck('product_uuid')
+            ->toArray();
+    }
+
+    public static function getRanges(string $currency)
+    {
+        $currency = Str::lower($currency);
+
+        if (!in_array($currency, config('theshop-campaign-manager.currencies'))) {
+            return [];
+        }
+
+        return CampaignManagerGift::query()
+            ->selectRaw("CAST(JSON_EXTRACT(minimum_spend, '$.eur') AS UNSIGNED) as range")
+            ->groupBy('range')
+            ->orderBy('range')
+            ->pluck('range')
+            ->toArray();
+    }
+}
